2097450dd459dbd55d0fb6db3df6fb438cb42383
AtsukiTak <takatomgoo@gmail.com>
2020-07-14 19:36:54 +0900

    panic::set_hook(Box::new(console_error_panic_hook::hook));

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -5,5 +5,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
     start::start()
 }
