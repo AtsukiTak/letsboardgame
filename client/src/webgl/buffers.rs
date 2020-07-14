c38c0a05ec49025ace7d5594b289ac7c5c1f0662
AtsukiTak <takatomgoo@gmail.com>
2020-07-14 22:05:43 +0900

    add IBO struct

--- client/src/webgl/buffers.rs
+++ client/src/webgl/buffers.rs
@@ -49,3 +49,54 @@ impl VBO {
         self.unbind();
     }
 }
+
+/// インデックスバッファーオブジェクト
+pub struct IBO {
+    buf: web_sys::WebGlBuffer,
+}
+
+impl IBO {
+    pub fn new() -> Self {
+        context::with(|ctx| {
+            // bufferの作成
+            let buf = ctx.create_buffer().unwrap();
+
+            IBO { buf }
+        })
+    }
+
+    pub fn with_data(data: &[i16]) -> Self {
+        let vbo = IBO::new();
+        vbo.set_data(data);
+        vbo
+    }
+
+    pub fn bind(&self) {
+        context::with(|ctx| {
+            ctx.bind_buffer(Context::ELEMENT_ARRAY_BUFFER, Some(&self.buf));
+        })
+    }
+
+    pub fn unbind(&self) {
+        context::with(|ctx| {
+            ctx.bind_buffer(Context::ELEMENT_ARRAY_BUFFER, None);
+        })
+    }
+
+    pub fn set_data(&self, data: &[i16]) {
+        self.bind();
+
+        context::with(|ctx| {
+            let js_array = js_sys::Int16Array::from(data);
+
+            // bufferにデータをセット
+            ctx.buffer_data_with_array_buffer_view(
+                Context::ELEMENT_ARRAY_BUFFER,
+                &js_array,
+                Context::STATIC_DRAW,
+            );
+        });
+
+        self.unbind();
+    }
+}
