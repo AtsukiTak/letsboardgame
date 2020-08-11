f0a33a9fa8d9893ab88f72334c1966c1f2035fbc
AtsukiTak <takatomgoo@gmail.com>
2020-08-11 20:01:50 +0900

    renderingに関する処理をrender_simple_objectにまとめる

--- src/renderer.rs
+++ src/renderer.rs
@@ -28,79 +28,82 @@ impl Renderer {
         // 背景色の設定
         context::clear_color(&self.scene.background);
 
-        self.program.use_program();
-
-        // ambient_color の設定
-        self.program
-            .params_mut()
-            .ambient_color
-            .set_value(self.scene.ambient_color.to_f32_vec4());
-
-        // eye_directionの設定
-        let eye_direction = self.camera.look_at - self.camera.pos;
-        self.program
-            .params_mut()
-            .eye_direction
-            .set_value(eye_direction);
-
-        // lightの設定
-        match self.scene.light {
-            Some(Light::Directional(ref light)) => {
-                let params = self.program.params_mut();
-                params.light_type.set_value(1);
-                params.light_val.set_value(light.dir);
-            }
-            Some(Light::Point(ref light)) => {
-                let params = self.program.params_mut();
-                params.light_type.set_value(2);
-                params.light_val.set_value(light.pos);
-            }
-            None => {
-                let params = self.program.params_mut();
-                params.light_type.set_value(0);
-            }
-        }
-
-        // カメラ周りの設定
-        let vp_matrix = self.camera.matrix();
-
         for object in self.scene.objects() {
-            // 各uniform変数の設定
-            let m_matrix = object.transform.matrix();
-            let params = self.program.params_mut();
-            params.m_matrix.set_value(m_matrix);
-            params.mvp_matrix.set_value(vp_matrix * m_matrix);
-            params.inv_m_matrix.set_value(m_matrix.invert().unwrap());
+            render_simple_object(&mut self.program, &self.scene, &self.camera, object);
+        }
+    }
+}
 
-            self.render_object(object);
+fn render_simple_object(
+    program: &mut BasicProgram,
+    scene: &Scene,
+    camera: &Camera,
+    object: &Object,
+) {
+    program.use_program();
+
+    // ambient_color の設定
+    program
+        .params_mut()
+        .ambient_color
+        .set_value(scene.ambient_color.to_f32_vec4());
+
+    // eye_directionの設定
+    let eye_direction = camera.look_at - camera.pos;
+    program.params_mut().eye_direction.set_value(eye_direction);
+
+    // lightの設定
+    match scene.light {
+        Some(Light::Directional(ref light)) => {
+            let params = program.params_mut();
+            params.light_type.set_value(1);
+            params.light_val.set_value(light.dir);
+        }
+        Some(Light::Point(ref light)) => {
+            let params = program.params_mut();
+            params.light_type.set_value(2);
+            params.light_val.set_value(light.pos);
+        }
+        None => {
+            let params = program.params_mut();
+            params.light_type.set_value(0);
         }
     }
 
-    fn render_object(&self, object: &Object) {
-        let mesh = &object.mesh;
+    // カメラ, Transform周りの設定
+    let vp_matrix = camera.matrix();
+    let m_matrix = object.transform.matrix();
+    program.params_mut().m_matrix.set_value(m_matrix);
+    program
+        .params_mut()
+        .mvp_matrix
+        .set_value(vp_matrix * m_matrix);
+    program
+        .params_mut()
+        .inv_m_matrix
+        .set_value(m_matrix.invert().unwrap());
 
-        // "position" attributeの設定
-        self.program
-            .params()
-            .position
-            .attach_vbo(&mesh.positions_vbo);
+    // 各attribute変数の設定
+    let mesh = &object.mesh;
 
-        // "normal" attributeの設定
-        self.program.params().normal.attach_vbo(&mesh.normals_vbo);
+    // "position" attributeの設定
+    program.params().position.attach_vbo(&mesh.positions_vbo);
 
-        // "color" attributeの設定
-        self.program.params().color.attach_vbo(&mesh.colors_vbo);
+    // "normal" attributeの設定
+    program.params().normal.attach_vbo(&mesh.normals_vbo);
 
-        // Index Bufferの設定
-        mesh.indexes_ibo.bind();
+    // "color" attributeの設定
+    program.params().color.attach_vbo(&mesh.colors_vbo);
 
-        context::with(|ctx| {
-            ctx.draw_elements_with_i32(
-                Context::TRIANGLES,
-                mesh.index_len,
-                Context::UNSIGNED_SHORT,
-                0,
-            );
-        })
-    }
+    // Index Bufferの設定
+    mesh.indexes_ibo.bind();
+
+    context::with(|ctx| {
+        ctx.draw_elements_with_i32(
+            Context::TRIANGLES,
+            mesh.index_len,
+            Context::UNSIGNED_SHORT,
+            0,
+        );
+    })
 }
