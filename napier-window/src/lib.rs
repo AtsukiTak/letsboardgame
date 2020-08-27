ef9c7f0d7d543cf59636ab2451e651c803aac269
AtsukiTak <takatomgoo@gmail.com>
2020-08-27 14:06:31 +0900

    add EventStream type

--- napier-window/src/lib.rs
+++ napier-window/src/lib.rs
@@ -1,7 +1,45 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    stream::Stream,
+    task::{Context, Poll},
+};
+use gloo_events::EventListener;
+use std::{borrow::Cow, pin::Pin};
+use web_sys::{Event, EventTarget};
+
+#[pin_project::pin_project]
+pub struct EventStream {
+    #[pin]
+    receiver: Receiver<Event>,
+    listener: EventListener,
+}
+
+impl EventStream {
+    pub fn listen<S>(target: &EventTarget, event_type: S) -> Self
+    where
+        S: Into<Cow<'static, str>>,
+    {
+        let (mut sender, receiver) = channel(1024);
+
+        let listener = EventListener::new(target, event_type, move |event| {
+            if let Err(_) = sender.try_send(event.clone()) {
+                log::info!("EventStream buffer is full");
+            }
+        });
+
+        EventStream { receiver, listener }
+    }
+}
+
+impl Stream for EventStream {
+    type Item = Event;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        let this = self.project();
+        this.receiver.poll_next(cx)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.receiver.size_hint()
     }
 }
